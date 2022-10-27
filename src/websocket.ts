2933df8434b9b72be32d61cb9af2d71a1ede7759
root <root@Julia.localdomain>
2022-10-27 00:56:21 -0300

    :rocket: Adds websocket and chat functionality

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,5 +1,64 @@
-import { io } from "./http"
+import { io } from "./http";
 
-io.on("connectioon", socket => {
-    console.log(socket.id)
+interface RoomUser {
+  socket_id: string;
+  username: string;
+  room: string;
+}
+
+interface Message {
+  room: string;
+  text: string;
+  createdAt: Date;
+  username: string;
+}
+
+const users: RoomUser[] = [];
+const messages: Message[] = [];
+
+// create server websocket
+io.on("connection", (socket) => {
+  socket.on("select_room", (data, callback) => {
+    socket.join(data.room);
+
+    const messagesRoom = getMessagesRoom(data.room);
+    const userInRoom = users.find((user) => {
+      user.room === data.room && user.username === data.username;
+    });
+
+    // retrieving messages sent in chat room
+    callback(messagesRoom);
+
+    if (userInRoom) {
+      userInRoom.socket_id = socket.id;
+      return;
+    }
+
+    users.push({
+      socket_id: socket.id,
+      username: data.username,
+      room: data.room,
+    });
+
+    socket.on("message", (data) => {
+      
+      // save messages
+      const message: Message = {
+        room: data.room,
+        username: data.username,
+        text: data.message,
+        createdAt: new Date(),
+      };
+
+      messages.push(message);
+
+      // send to room users
+      io.to(data.room).emit("message", message);
+    });
+  });
 });
+
+function getMessagesRoom(room: string) {
+    const messagesRoom = messages.filter((message) => message.room === room);
+    return messagesRoom;
+}
